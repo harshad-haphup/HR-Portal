84bddc15aae3b53ac6a430f0e506133929b3b534
harshad-27 <harshadshinde118@gmail.com>
2023-03-20 11:05:58 +0530

    User Controller Work

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,24 +7,24 @@ class UserController < ApplicationController
     end
 
     def index
-        user=User.all
+        users=User.all
+        render json:{'users':users}, status: :ok
+    end
+
+    def show
+        user=User.find(params[:id])
         render json:{'user':user}, status: :ok
     end
 
-    # def show
-    #     task=Task.find(params[:id])
-    #     render json:{'task':task}, status: :ok
-    # end
+    def update
+        user=User.find(params[:id])
+        user.update({first_name:params[:first_name],middle_name:params[:middle_name],last_name:params[:last_name],email:params[:email],contact_no:params[:contact_no],address:params[:address],job_profile:params[:job_profile],is_admin:params[:is_admin],salary:params[:salary]})
+        render json:{'user':user}, status: :ok
+    end
 
-    # def update
-    #     task=Task.find(params[:id])
-    #     task.update({name:params[:name],description:params[:description],iscompleted:params[:iscompleted]})
-    #     render json:{'task':task}, status: :ok
-    # end
-
-    # def destroy
-    #     Task.destroy(params[:id])
-    #     task=Task.all
-    #     render json:{'tasks':task}, status: :ok
-    # end
+    def destroy
+        User.destroy(params[:id])
+        users=User.all
+        render json:{'users':users}, status: :ok
+    end
 end
