f50443def59f3db056f32c13881a14ce728a57e5
harshad-27 <harshadshinde118@gmail.com>
2023-03-20 15:16:01 +0530

    Add Validations for user Form

--- app/javascript/packs/components/AddUser.jsx
+++ app/javascript/packs/components/AddUser.jsx
@@ -17,70 +17,154 @@ import {
 import React from "react";
 import Header from "./Header";
 import { Person2Outlined, Person } from "@mui/icons-material";
+import { useForm } from "react-hook-form";
 
 const AddUser = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    saveData(data)
+  };
+  const saveData = async (data)=>{
+    
+  }
   return (
     <>
       <Header />
       <Container className="m-8">
-        <Stack spacing={2} className="mx-0 sm:mx-8 md:mx-16 mb-4">
-          <Box>
-            <Typography
-              className="font-bold text-lg border-l-4 px-2 border-blue-500 bg-blue-100 max-w-max rounded-sm"
-              variant="h6"
-            >
-              General Information
-            </Typography>
-            <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
-              <TextField label="First Name" variant="outlined" fullWidth />
-              <TextField label="Middle Name" variant="outlined" fullWidth />
-              <TextField label="Last Name" variant="outlined" fullWidth />
+        <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+          <Stack spacing={2} className="mx-0 sm:mx-8 md:mx-16 mb-4">
+            <Box>
+              <Typography
+                className="font-bold text-lg border-l-4 px-2 border-blue-500 bg-blue-100 max-w-max rounded-sm"
+                variant="h6"
+              >
+                General Information
+              </Typography>
+              <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
+                <TextField
+                  label="First Name*"
+                  variant="outlined"
+                  fullWidth
+                  {...register("first_name", {
+                    required: "First name is required",
+                  })}
+                  error={Boolean(errors.first_name)}
+                  helperText={errors.first_name?.message}
+                />
+
+                <TextField
+                  label="Middle Name*"
+                  variant="outlined"
+                  fullWidth
+                  {...register("middle_name", {
+                    required: "Middle name is Required",
+                  })}
+                  error={Boolean(errors.middle_name)}
+                  helperText={errors.middle_name?.message}
+                />
+                <TextField
+                  label="Last Name*"
+                  variant="outlined"
+                  fullWidth
+                  {...register("last_name", { required: "Last name is required" })}
+                  error={Boolean(errors.last_name)}
+                  helperText={errors.last_name?.message}
+                />
+              </Box>
             </Box>
-          </Box>
-          {/* Second Row */}
-          <Box>
-            <Typography
-              className="font-bold text-lg border-l-4 px-2 mt-4 border-blue-500 bg-blue-100 max-w-max rounded-sm"
-              variant="h6"
-            >
-              Contact Information
-            </Typography>
-            <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
-              <TextField label="Contact No" variant="outlined" fullWidth />
-              <TextField label="Email" variant="outlined" fullWidth />
-              <TextField label="Address" variant="outlined" fullWidth />
+            {/* Second Row */}
+            <Box>
+              <Typography
+                className="font-bold text-lg border-l-4 px-2 mt-4 border-blue-500 bg-blue-100 max-w-max rounded-sm"
+                variant="h6"
+              >
+                Contact Information
+              </Typography>
+              <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
+                <TextField 
+                label="Contact No*" 
+                variant="outlined" 
+                fullWidth
+                {...register("contact_no", { required: "Contact no is required" })}
+                error={Boolean(errors.contact_no)}
+                helperText={errors.contact_no?.message}
+                />
+                <TextField 
+                label="Email*" 
+                variant="outlined" 
+                fullWidth
+                {...register("email", { required: "Email no is required" })}
+                error={Boolean(errors.email)}
+                helperText={errors.email?.message}
+                />
+                <TextField 
+                label="Address*" 
+                variant="outlined" 
+                fullWidth
+                {...register("address", { required: "Address no is required" })}
+                error={Boolean(errors.address)}
+                helperText={errors.address?.message}
+                />
+              </Box>
             </Box>
-          </Box>
-          {/* Third Row */}
-          <Box>
-            <Typography
-              className="font-bold text-lg border-l-4 mt-4 px-2 border-blue-500 bg-blue-100 max-w-max rounded-sm"
-              variant="h6"
-            >
-              Other Information
-            </Typography>
-            <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
-              <TextField label="Job Profile" variant="outlined" fullWidth />
-              <TextField label="Salary" variant="outlined" fullWidth />
-              <FormControl fullWidth>
-                <Box className="flex items-center  p-1 rounded">
-                  <FormControlLabel
-                    label={<Typography className="text-gray-500">Is Admin</Typography>}
-                    control={
-                      <Checkbox
-                        icon={<Person2Outlined/>}
-                        checkedIcon={<Person/>}
-                      />
-                    }
-                  />
-                </Box>
-              </FormControl>
+            {/* Third Row */}
+            <Box>
+              <Typography
+                className="font-bold text-lg border-l-4 mt-4 px-2 border-blue-500 bg-blue-100 max-w-max rounded-sm"
+                variant="h6"
+              >
+                Other Information
+              </Typography>
+              <Box className="flex flex-col gap-4 mt-3 mx-2 md:flex-row">
+                <TextField 
+                label="Job Profile*" 
+                variant="outlined" 
+                fullWidth
+                {...register("job_profile", { required: "Job Profile no is required" })}
+                error={Boolean(errors.job_profile)}
+                helperText={errors.job_profile?.message}
+                />
+                <TextField 
+                label="Salary*" 
+                variant="outlined" 
+                fullWidth
+                {...register("salary", { required: "Salary no is required" })}
+                error={Boolean(errors.salary)}
+                helperText={errors.salary?.message}
+                />
+                <FormControl fullWidth>
+                  <Box className="flex items-center  p-1 rounded">
+                    <FormControlLabel
+                      label={
+                        <Typography className="text-gray-500">
+                          Is Admin
+                        </Typography>
+                      }
+                      control={
+                        <Checkbox
+                          icon={<Person2Outlined />}
+                          checkedIcon={<Person />}
+                          {...register("is_admin", { required: false })}
+                        />
+                      }
+                    />
+                  </Box>
+                </FormControl>
+              </Box>
             </Box>
-          </Box>
-          <Box >
-            <Button variant="contained">Add User</Button>
-          </Box>
-        </Stack>
+            {/* Submit Button */}
+            <Box>
+              <Button variant="contained" type="submit">
+                Add User
+              </Button>
+            </Box>
+          </Stack>
+        </form>
       </Container>
     </>
   );
